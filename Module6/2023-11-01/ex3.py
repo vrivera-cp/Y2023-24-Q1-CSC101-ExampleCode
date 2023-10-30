1b5ffd557b7053ba55456581fbf389729cf8b6db
Vanessa Rivera <10712121+rivessa@users.noreply.github.com>
2023-10-30 16:37:00 -0700

    Fix formatting in Module6/ex3.py

--- Module6/2023-11-01/ex3.py
+++ Module6/2023-11-01/ex3.py
@@ -14,6 +14,7 @@ def get_direction(v: Vector) -> float:
     """Return the direction of the vector in degrees."""
     return atan2(v.y, v.x) * 180 / pi
 
+
 if __name__ == '__main__':
     vector = Vector(1.0, 1.0)
 

a7e209e32e4835bb0faed0777e0118abcce4606b
Vanessa Rivera <10712121+rivessa@users.noreply.github.com>
2023-11-05 06:42:09 -0800

    fix file_parsing_2.py

--- Module8/1-Reading-Files/file_parsing_2.py
+++ Module8/1-Reading-Files/file_parsing_2.py
@@ -1,4 +1,4 @@
-"""file_parsing_1.py
+"""file_parsing_2.py
 Example of converting text to numerics.
 """
 
@@ -8,8 +8,6 @@ if __name__ == '__main__':
     with open('data_bad.csv') as f:
         next(f)  # Skips the first line
         for line in f:
-            print(f'Line {i}')
-
             line = line.strip()  # Remove trailing new lines
 
             data = line.split()  # Obtain a list of strings, separated by whitespace in the original string
@@ -17,7 +15,7 @@ if __name__ == '__main__':
             x = float(data[0])
             y = float(data[1])
 
-            xs.append(float(data[0]))  # first column
-            ys.append(float(data[1]))  # second column
+            xs.append(x)  # first column
+            ys.append(y)  # second column
     print("x values:", xs)
     print("y values:", ys)
